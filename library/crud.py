25dd0df2733f6f779266e461c3eea35cadf000d3
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-09-08 19:42:53 +0300

    Add files via upload

--- library/crud.py
+++ library/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 import models, schemas
-
+from passlib.context import CryptContext
 
 # ----- Author -----
 
@@ -41,3 +41,42 @@ def create_book(db: Session, book: schemas.BookCreate):
     db.refresh(db_book)
     return db_book
 
+# ----- User -----
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_user(db: Session, login: str):
+    return db.query(models.User).filter(models.User.login == login).first()
+
+
+def authenticate_user(db: Session, login: str, password: str):
+    user = get_user(db, login)
+    if not user:
+        return False
+    # додаємо сіль перед перевіркою
+    if not verify_password(password + user.salt, user.password):
+        return False
+    return user
+
+
+def create_user(db: Session, login: str, password: str):
+    salt = secrets.token_hex(16)
+    hashed_password = get_password_hash(password + salt)
+
+    db_user = models.User(
+        login=login,
+        password=hashed_password,  # тут зберігається саме hash
+        salt=salt,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
